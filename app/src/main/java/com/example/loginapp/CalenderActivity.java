b2a60279fb081693058f232ff118050237b94d0a
PoojithaYanda <62445864+PoojithaYanda@users.noreply.github.com>
2020-08-04 16:47:53 -0500

    Changes include to manage the Project.java file and Calender

---
+++ app/src/main/java/com/example/loginapp/CalenderActivity.java
@@ -0,0 +1,105 @@
+package com.example.loginapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CalendarView;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import com.example.loginapp.model.Project;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+public class CalenderActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+    CalendarView calendarView;
+    TextView myDate;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calender);
+
+        toolbar = findViewById(R.id.toolbar);
+
+        calendarView = (CalendarView) findViewById(R.id.calendarView);
+        myDate = (TextView) findViewById(R.id.myDate);
+
+        Date currentDate = new Date(calendarView.getDate());
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        renderProjectsForDate(dateFormat.format(currentDate));
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int i, int i1, int i2) {
+                String date = String.format("%02d/%02d/%04d", (i1 + 1), i2, i);
+                myDate.setText(date);
+                renderProjectsForDate(date);
+            }
+        });
+    }
+
+    private void renderProjectsForDate(String date) {
+        // Get All Projects for User
+        final List<Project> projects = Project.getAllProjects(AppState.getUserInstance().getUserId());
+
+        final TableLayout tableLayout = (TableLayout) findViewById(R.id.projectsListLayout);
+        TableRow headerRow =  (TableRow) tableLayout.getChildAt(0);
+        tableLayout.removeAllViews();
+        tableLayout.addView(headerRow);
+
+        int counter=0;
+        for(int j=0;j<projects.size();j++) {
+            final Project thisProject = projects.get(j);
+            Log.d(ProjectActivity.class.getName(), thisProject.toString());
+            if(date.equals(thisProject.getDate())) {
+                counter++;
+                TableRow newRow = new TableRow(CalenderActivity.this);
+                newRow.setPadding(10, 10, 10, 10);
+                newRow.setBackgroundColor(Color.parseColor("#F1F1F1"));
+
+                final TextView nameView = new TextView(calendarView.getContext());
+                TableRow.LayoutParams nameParams = new TableRow.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, 1.0f);
+                nameView.setLayoutParams(nameParams);
+                nameView.setText(projects.get(j).getProjectName());
+                nameView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent intent = new Intent(CalenderActivity.this, ChecklistActivity.class);
+                        intent.putExtra("projectId", String.valueOf(thisProject.getpId()));
+                        intent.putExtra("projectName", thisProject.getProjectName());
+                        startActivity(intent);
+                    }
+                });
+
+                newRow.addView(nameView);
+                tableLayout.addView(newRow, counter);
+            }
+        }
+        if(counter==0) {
+            TableRow newRow = new TableRow(CalenderActivity.this);
+            newRow.setPadding(10, 10, 10, 10);
+            newRow.setBackgroundColor(Color.parseColor("#F1F1F1"));
+            final TextView nameView = new TextView(calendarView.getContext());
+            TableRow.LayoutParams nameParams = new TableRow.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, 1.0f);
+            nameView.setLayoutParams(nameParams);
+            nameView.setText("No Projects for this day");
+            newRow.addView(nameView);
+            tableLayout.addView(newRow, 1);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/loginapp/model/Project.java
@@ -0,0 +1,113 @@
+package com.example.loginapp.model;
+
+import android.util.Log;
+
+import com.example.loginapp.RestAPIClient;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Project {
+
+    private static String BASE_URL = "http://10.0.2.2:8080/";
+
+    private int userId;
+    private int pId;
+    private String customerName;
+    private String customerEmail;
+    private String projectName;
+    private String date;
+
+    public int getUserId() { return this.userId;}
+    public int getpId() { return this.pId;}
+    public String getCustomerName() { return this.customerName;}
+    public String getCustomerEmail() { return this.customerEmail;}
+    public String getProjectName() { return this.projectName;}
+    public String getDate() { return this.date;}
+
+    public Project() {
+    }
+
+    public Project(String name, String date) {
+        this.projectName = name;
+        this.date = date;
+    }
+
+    public Project getProject(int pId) {
+        String GET_PROJECT_URL = String.format(BASE_URL + "project?id=%s",pId);
+        try {
+            String result = new RestAPIClient().execute(GET_PROJECT_URL).get();
+            Log.d("Project Model", result);
+            convertToProject(result);
+        } catch (Exception e) {
+            Log.e("Project Model", "Unable to get  the project data:", e);
+            e.printStackTrace();
+        }
+        return this;
+    }
+
+    public Project createProject(int userId, String projectName, String customerName, String customerEmail, String date) {
+        String CREATE_PROJECT_URL = String.format(BASE_URL + "user/%s/project/create?projectName=%s&customerName=%s&customerEmail=%s&date=%s", userId, projectName, customerName, customerEmail, date);
+        try {
+            String result = new RestAPIClient().execute(CREATE_PROJECT_URL).get();
+            Log.d("Project Model", result);
+            convertToProject(result);
+        } catch (Exception e) {
+            Log.e("Project Model", "Unable to create a Project", e);
+            e.printStackTrace();
+        }
+        return this;
+    }
+
+    public static List<Project> getAllProjects(int userId) {
+        String GET_ALL_PROJECTS_URL = String.format(BASE_URL + "user/%s/projects", userId);
+        List<Project> projects= new ArrayList<>();
+        try {
+            String result = new RestAPIClient().execute(GET_ALL_PROJECTS_URL).get();
+            projects =  convertToList(result);
+        } catch (Exception e) {
+            Log.e("Project Model", "Unable to get all projects for user = " + userId, e);
+        }
+
+        return projects;
+    }
+
+    private void convertToProject(String jsonString) throws Exception {
+        Log.d(Project.class.getName(), "Results - convertToProject : " + jsonString);
+        JSONObject jsonObject = new JSONObject(jsonString);
+        this.userId = jsonObject.getInt("userId");
+        this.customerEmail = jsonObject.getString("customerEmail");
+        this.customerName = jsonObject.getString("customerName");
+        this.projectName = jsonObject.getString("projectName");
+        this.date = jsonObject.getString("date");
+        this.pId = jsonObject.getInt("pId");
+    }
+
+    private static List<Project> convertToList(String jsonString) throws Exception {
+        Log.d(Project.class.getName(), "Results - convertToList : " + jsonString);
+        JSONArray array = new JSONArray(jsonString);
+        List<Project> projects = new ArrayList<>();
+        for (int i=0 ; i<array.length();i++) {
+            JSONObject obj = array.getJSONObject(i);
+            Project newProject = new Project();
+            newProject.userId = obj.getInt("userId");
+            newProject.customerEmail = obj.getString("customerEmail");
+            newProject.customerName = obj.getString("customerName");
+            newProject.projectName = obj.getString("projectName");
+            newProject.date = obj.getString("date");
+            newProject.pId = obj.getInt("pId");
+            projects.add(newProject);
+        }
+
+        return projects;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Project [pId = %s, name = %s, uId = %s, date = %s]", pId, projectName, userId, date);
+    }
+
+}
